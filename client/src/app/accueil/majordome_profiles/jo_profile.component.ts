94335f4af54bcee1a1b636b4d0636ba6880c1a4b
Unknown <mghounto@gmail.com>
2018-01-21 10:29:27 -0500

    younes

---
+++ client/src/app/accueil/majordome_profiles/jo_profile.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-joProfile',
+  templateUrl: './jo_profile.component.html',
+  styleUrls: ['./jo_profile.component.css']
+})
+export class JoProfileComponent {
+    constructor() { }
+}
