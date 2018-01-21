04e8ea6885704e18af521bf34a50369287efdac4
Unknown <mghounto@gmail.com>
2018-01-20 22:33:45 -0500

    page daccueil prototype

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { AnimauxComponent } from './ServiceAnimaux/animaux.component';
+import { AccueilComponent } from './accueil/accueil.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
-
 import {EpicerieComponent} from './serviceEpicerie/epicerie.component';
 
 import { AppComponent } from './app.component';
@@ -11,14 +11,16 @@ import { AppComponent } from './app.component';
 
 const appRoutes: Routes = [
   { path: 'epicerie', component: EpicerieComponent },
-  { path: 'animaux', component: AnimauxComponent }
+  { path: '', component: AccueilComponent},
+  { path: 'animaux', component: AnimauxComponent },
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     EpicerieComponent,
-    AnimauxComponent
+    AnimauxComponent,
+    AccueilComponent,
   ],
   imports: [
     RouterModule.forRoot(
