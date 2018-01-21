99adc9f322a71cadaf2ec75ca285835e13073c65
Unknown <younes.ijichi@gmail.com>
2018-01-21 09:23:07 -0500

    objet Garderie dans le form

--- client/src/app/ServiceGarderie/garderie.model.ts
+++ client/src/app/ServiceGarderie/garderie.model.ts
@@ -1,3 +1,15 @@
 export class Garderie {
-    
+    public nombre: number;
+    public adresse: string;
+    public debut: number;
+    public fin: number;
+    public commentaires: string;
+
+    /*constructor (nombre: number, adresse: string, debut: number, fin: number, commentaires: string) {
+        this.nombre = nombre;
+        this.adresse = adresse;
+        this.debut = debut;
+        this.fin = fin;
+        this.commentaires = commentaires;
+    }*/
 }
--- client/src/app/ServiceGarderie/serviceGarderie.component.ts
+++ client/src/app/ServiceGarderie/serviceGarderie.component.ts
@@ -1,3 +1,4 @@
+import { Garderie } from './garderie.model';
 import { Component } from '@angular/core';
 import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 import * as socketIO from 'socket.io-client';
@@ -8,12 +9,16 @@ import * as socketIO from 'socket.io-client';
   styleUrls: ['./serviceGarderie.component.css']
 })
 export class ServiceGarderieComponent implements OnInit {
-
   private socket: socketIO.Socket;
+  private garderie: Garderie;
 
   ngOnInit() {
+    this.garderie = new Garderie();
     this.socket = socketIO.connect('10.200.10.215:3000');
   }
 
-  
+  private sauvegarderGarderie() {
+    console.log('garderie : ', this.garderie);
+    
+  }
 }
